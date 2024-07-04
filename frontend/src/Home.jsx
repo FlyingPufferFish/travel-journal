ad38ed8b9696700c893e38d4d469ede31942174e
Eden Xu <130119691+FlyingPufferFish@users.noreply.github.com>
2024-07-04 10:30:26 +1000

    Frontend: correctly rendering fetched data in PoiBoard component

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -22,6 +22,7 @@ export function PoiBoard(props) {
 
   useEffect(() => {
     async function fetchData() {
+      // console log the destination url 
       console.log(import.meta.env.VITE_API_URL)
       try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}pois/`);
@@ -30,6 +31,7 @@ export function PoiBoard(props) {
         }
         const result = await response.json();
         console.log(result)
+        // update 'data': set the value of 'data' to the value of 'result'
         setData(result);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -43,7 +45,13 @@ export function PoiBoard(props) {
     // <section id= 'about' className={styles.about}>
     <section id='poiboard'>
         <div>
-            PoI Board Placeholder
+            {/* display all POI */}
+            {data.map((item) => (
+              <div>
+                <p key={item.id}>{item.title}</p>
+                <img src={item.image}/>
+              </div>
+            ))}
         </div>
     </section>
   );
